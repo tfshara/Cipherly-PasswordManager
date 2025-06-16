06529b3705a4b59d279ff16a1c78f2eab55c2e99
Shara trikha <115238311+tfshara@users.noreply.github.com>
2025-06-16 22:50:19 +0530

    Update vite.config.js

--- PasswordManager/vite.config.js
+++ PasswordManager/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // this tells Vite to look in the root directory for index.html
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
 })
